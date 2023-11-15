5e71e746565c5e45894ee18dd67574ddbe8a2a42
Noah Teo <75418081+naozitios@users.noreply.github.com>
2023-11-15 19:30:17 +0800

    testing tutorial

--- test/Token.js
+++ test/Token.js
@@ -15,3 +15,12 @@ Ticket buyer can buy tickets from the initial sale
 Ticket owner can list their ticket on the secondary market
 Another ticket buyer can buy the ticket from the secondary market
 */
+
+/*
+Steps to initiate
+1. Create multiple ticket contracts
+2. Pool the ticket contracts into ticketmaster
+3. Buy tickets
+4. List tickets onto the marketplace
+5. Get someone else to buy a ticket off the marketplace
+*/
